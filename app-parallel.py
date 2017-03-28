0d1fb6c712d04ee1240da872f2868d8148a1b9df
Allen Geer <me@allengeer.com>
2017-03-28 13:52:06 -0700

    Updates to allow multiple ticker processing and more tracking/logging/caching in Redis sidecar

--- app-parallel.py
+++ app-parallel.py
@@ -10,19 +10,21 @@ import redis
 lock = Lock()
 redisCache = redis.StrictRedis(host='localhost', port=6379, db=0)
 
-def scrapeAnalyze(link):
+def scrapeAnalyze(ticker, link):
     sha = hashlib.md5(link).hexdigest()
     content = redisCache.get(sha)
     if content is None:
         q = requests.get("http://localhost:5001/scrape?url=%s" % link)
         content = q.content.decode('unicode_escape').encode('ascii', 'ignore')
         redisCache.set(sha,content)
+    redisCache.set("%s:%s:CONTENT" %(ticker, link), content)
 
     payload = {"text": content}
     headers = {'Content-Type': 'application/json'}
-    print "%s Start\t" %link
+    #print "%s Start\t" %link
     z = requests.post("http://localhost:5002/analyze", json=json.dumps(payload), headers=headers)
-    print "STOP %s Stop" %link
+    redisCache.set("%s:%s:TONE" %(ticker, link), z.content)
+    #print "STOP %s Stop" %link
     jsoninfo = z.json()
 
     if 'document_tone' in jsoninfo:
@@ -39,49 +41,49 @@ if __name__ == '__main__':
     start = time.time()
 
     if len(sys.argv) > 1:
-        ticker = sys.argv[1]
-    else:
-        ticker = "SNAP"
+        for idx in range(1,len(sys.argv)):
+            ticker = sys.argv[idx]
 
-    r = requests.get("http://localhost:5000/news?ticker=%s" %ticker)
-    listOfLinks = r.json()
+            r = requests.get("http://localhost:5000/news?ticker=%s" %ticker)
+            listOfLinks = r.json()
 
-    emotions = dict()
-    pricing = requests.get("http://localhost:5003/stock/%s" % ticker)
-    pricing = pricing.json()
-    print "============ %s =============" %time.strftime("%c")
-    print pricing['symbol'], pricing["price"], pricing["name"]
-    print "======= Running analysis on %d links =======" %len(listOfLinks)
+            emotions = dict()
+            pricing = requests.get("http://localhost:5003/stock/%s" % ticker)
+            pricing = pricing.json()
+            #print "============ %s =============" %time.strftime("%c")
+            #print pricing['symbol'], pricing["price"], pricing["name"]
+            #print "======= Running analysis on %d links =======" %len(listOfLinks)
 
-    threads = []
-    for link in listOfLinks:
-        t = threading.Thread(target=scrapeAnalyze, args=(link,))
-        threads.append(t)
-        t.start()
-        # t.join()
+            threads = []
+            for link in listOfLinks:
+                t = threading.Thread(target=scrapeAnalyze, args=(ticker, link,))
+                threads.append(t)
+                t.start()
+                # t.join()
 
-    for t in threads:
-        t.join()
+            for t in threads:
+                t.join()
 
-    emotions["Normal"] = np.random.normal(0.5, 0.341, len(listOfLinks))
+            emotions["Normal"] = np.random.normal(0.5, 0.341, len(listOfLinks))
 
-    # print "%s %s (%s)" %pricing["price"], pricing["name"], pricing["symbol"]
-    print "%sMean\tVar\t\tStd\t\t\t(%s samples)" %('{0: <20}'.format('Emotion'),len(emotions.get("Anger")))
-    for key,value in emotions.iteritems():
-        u = np.mean(value)
-        var = np.var(value)
-        std = np.std(value)
-        print "%s%.4f\t%.4f\t%.4f" %('{0: <20}'.format(key), u, var, std)
+            # print "%s %s (%s)" %pricing["price"], pricing["name"], pricing["symbol"]
+            # print "%sMean\tVar\t\tStd\t\t\t(%s samples)" %('{0: <20}'.format('Emotion'),len(emotions.get("Anger")))
+            # for key,value in emotions.iteritems():
+            #     u = np.mean(value)
+            #     var = np.var(value)
+            #     std = np.std(value)
+            #     print "%s%.4f\t%.4f\t%.4f" %('{0: <20}'.format(key), u, var, std)
 
-    print "%s\t%s\t" %(time.strftime("%c"), pricing["price"]),
-    for key, value in emotions.iteritems():
-        u = np.mean(value)
-        std = np.std(value)
-        if key is not "Normal":
-            print "%.4f\t%.4f\t" %(u, std),
+            print "%s\t%s\t%s\t" %(ticker, time.strftime("%c"), pricing["price"]),
+            for key, value in emotions.iteritems():
+                u = np.mean(value)
+                std = np.std(value)
+                if key is not "Normal":
+                    print "%.4f\t%.4f\t" %(u, std),
+            print ""
 
-    end = time.time()
+            end = time.time()
 
-    elapsed = end - start
-    print ""
-    print "ELAPSED TIME %s" %elapsed
+            elapsed = end - start
+            #print ""
+            #print "%s ELAPSED TIME %s" %(ticker, elapsed)
